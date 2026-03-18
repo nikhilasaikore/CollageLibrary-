6c3c255c8b434ce17358a090ea734d1ad09d2cb0
nikhilasaikore <nikhilasaikore@gmail.com>
2026-03-18 12:40:52 +0530

    Update library list.js

--- library list.js
+++ library list.js
@@ -67,12 +67,12 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     const u = document.getElementById('username').value;
     const p = document.getElementById('password').value;
 
-    if(u === 'admin' && p === 'admin123') {
+    if(u === 'nikhila' && p === 'nikhila123') {
         // FIX: Store login state in sessionStorage
         sessionStorage.setItem('isLoggedIn', 'true');
         showDashboard();
     } else {
-        alert("Invalid Credentials! Try admin / admin123");
+        alert("Invalid Credentials! Try nikhila / nikhila");
     }
 });
 
